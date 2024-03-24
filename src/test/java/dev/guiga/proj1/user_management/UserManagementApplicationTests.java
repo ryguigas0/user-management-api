600cb6e0744600f2b515216c0b24480c734c8223
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2024-03-24 20:15:50 -0300

    created project

---
+++ src/test/java/dev/guiga/proj1/user_management/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guiga.proj1.user_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
